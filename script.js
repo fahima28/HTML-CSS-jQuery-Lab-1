433687e345fe463c3f5a4e1cc1387d885b7bca0d
Fahim A. <147541831+fahima28@users.noreply.github.com>
2026-01-04 19:59:27 -0500

    Add interactive buttons for media control

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+$(function () {
+  $("#videoBox").hide();
+
+  $("#bgBtn").on("click", function () {
+    $("body").toggleClass("alt-bg");
+  });
+
+  $("#imgBtn").on("click", function () {
+    $("#missionImage").slideToggle(200);
+  });
+
+  $("#audioBtn").on("click", function () {
+    const audio = $("#storyAudio").get(0);
+    if (audio.paused) audio.play();
+    else audio.pause();
+  });
+
+  $("#videoBtn").on("click", function () {
+    $("#videoBox").slideToggle(200, function () {
+      const video = $("#habitatVideo").get(0);
+      if ($(this).is(":visible")) video.play();
+      else {
+        video.pause();
+        video.currentTime = 0;
+      }
+    });
+  });
+});
 
